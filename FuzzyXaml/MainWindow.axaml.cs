17be4961449b006be73203bc719f261400a3571f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-12-20 20:21:01 +0100

    Generate and execute test cases

--- FuzzyXaml/MainWindow.axaml.cs
+++ FuzzyXaml/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Input;
@@ -8,25 +9,63 @@ namespace FuzzyXaml;
 
 public abstract class TestAction
 {
+    public abstract void Execute(object control);
 }
 
 public class ButtonExecuteCommandTestAction : TestAction
 {
-    
+    public object? Parameter { get; set; }
+
+    public ButtonExecuteCommandTestAction(object? parameter)
+    {
+        Parameter = parameter;
+    }
+
+    public override void Execute(object control)
+    {
+        Console.WriteLine($"[{nameof(Button)}.{nameof(Button.Command)}]");
+
+        if (control is Button button && button.IsEnabled && button.Command is { })
+        {
+           button.Command.Execute(Parameter);
+        }  
+    }
 }
 
 public class TextBoxSetTextPropertyTestAction : TestAction
 {
-    
+    public string? Text { get; set; }
+
+    public TextBoxSetTextPropertyTestAction(string? text)
+    {
+        Text = text;
+    }
+
+    public override void Execute(object control)
+    {
+        Console.WriteLine($"[{nameof(TextBox)}.{nameof(TextBox.Text)}]");
+
+        if (control is TextBox textBox && textBox.IsEnabled)
+        {
+            textBox.Text = Text;
+        }  
+    }
 }
 
-public class TesCase
+public class TestCase
 {
     public int Level { get; set; }
 
     public int Index { get; set; }
-    
-    public TestAction Action { get; set; }
+
+    public TestAction? Action { get; set; }
+
+    public TestCase(int level, int index, TestAction? action)
+    {
+        Level = level;
+        Index = index;
+        Action = action;
+    }
 }
 
 public partial class MainWindow : Window
@@ -42,43 +81,65 @@ public partial class MainWindow : Window
     {
         if (e.Key == Key.F5)
         {
-            int level = 0;
-            
-            var controls = this.GetVisualDescendants().OfType<Control>().ToList();
-            for (var index = 0; index < controls.Count; index++)
+            var testCases = GenerateTestCases();
+            if (testCases.Count > 0)
             {
-                var control = controls[index];
-                // Console.WriteLine($"{control}");
+                ExecuteTestCases(testCases);
+            }
+        }
+    }
 
-                // TODO: use tab order
-                // TODO: generate paths for testing
-                // TODO: use random order
+    private void ExecuteTestCases(List<TestCase> testCases)
+    {
+        var level = 0;
+        var controls = this.GetVisualDescendants().OfType<Control>().ToList();
 
-                switch (control)
+        for (var index = 0; index < controls.Count; index++)
+        {
+            var control = controls[index];
+
+            // Console.WriteLine($"[{level}:{index}] {control.GetType().FullName}");
+
+            foreach (var testCase in testCases)
+            {
+                if (testCase.Level == level && testCase.Index == index)
                 {
-                    case TextBox textBox:
-                    {
-                        Console.WriteLine($"[{level}:{index}] Set {nameof(TextBox)}.Text");
-                        textBox.Text = "Random Text";
-                        // TODO: record TextBoxSetTextPropertyTestAction
-                        // TODO: after recording action check level ? record next action ? check tree diff ?
-                        break;
-                    }
-                    case Button button:
-                    {
-                        if (button.IsEnabled && button.Command is { })
-                        {
-                            Console.WriteLine($"[{level}:{index}] Execute {nameof(Button)}.Command");
-                            button.Command.Execute(null);
-                            // TODO: record ButtonExecuteCommandTestAction
-                            // TODO: after recording action check level ? record next action ? check tree diff ?
-                        }
-
-                        break;
-                    }
+                    testCase.Action?.Execute(control);
                 }
             }
         }
+    }
+
+    private List<TestCase> GenerateTestCases()
+    {
+        var testCases = new List<TestCase>();
+        var level = 0;
+        var controls = this.GetVisualDescendants().OfType<Control>().ToList();
+
+        for (var index = 0; index < controls.Count; index++)
+        {
+            var control = controls[index];
+
+            // Console.WriteLine($"[{level}:{index}] {control.GetType().FullName}");
+
+            switch (control)
+            {
+                case TextBox:
+                {
+                    // Console.WriteLine($"[{nameof(TextBox)}.{nameof(TextBox.Text)}]");
+                    testCases.Add(new TestCase(level, index, new TextBoxSetTextPropertyTestAction("Random Text")));
+                    break;
+                }
+                case Button:
+                {
+                    // Console.WriteLine($"[{nameof(Button)}.{nameof(Button.Command)}]");
+                    testCases.Add(new TestCase(level, index, new ButtonExecuteCommandTestAction(null)));
+                    break;
+                }
+            }
+        }
+
+        return testCases;
     }
 
     public void SomeMethod()
